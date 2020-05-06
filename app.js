178383fab2fb97800af49c596c5f2f85ec9a5db5
Lucas-antonin <53711285+Lucas-antonin@users.noreply.github.com>
2020-05-06 16:59:04 -0300

    About the repository
    My First website

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+
+app.use(express.static(__dirname + "/public"))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + "/app/views/index.html")
+})
+app.get("/pictures",(req,res)=>{
+    res.sendFile(__dirname + "/app/views/index2.html")
+})
+
+app.get("/album", (req,res)=>{
+    res.sendFile(__dirname + '/app/views/index3.html')
+})
+
+app.listen(8081, ()=>{
+    console.log("Server have been opened")
+})
