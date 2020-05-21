746e2e7eda719a5941690def4452592a2ea5f642
Lucas-antonin <53711285+Lucas-antonin@users.noreply.github.com>
2020-05-21 15:47:29 -0300

    Lists witwh ejs(not complete)
    just add a page with 2 different lists using ejs.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const app = express()
+//variables
+let foxEat = [{ name:"Leporids"}, { name:"Raccoons"}, { name:"Squirrels"}, { name:"Hamster"}, { name:"Gerbils"}, { name:"Gopher"}, { name:"Arvicolinae"}, { name:"Murids"}, { name:"Opossums"}]
+let species = [{ name:"Red Fox"}, {name: "Bengal Fox"}, {name: "Rüppell's Fox"}, {name: "Kit Fox"},{name: "Swift Fox"},{name:"Tibetan Sand Fox"}, {name:"Corsac Fox"}, {name:"Cape Fox"}, {name:"Blanford's Fox"}, {name:"Pale Fox"}, {name:"Fennec Fox"}, {name:"Arctic Fox"}]
+let data = {a:foxEat,b:species}
 
 app.use(express.static(__dirname + "/public"))
 
@@ -16,6 +20,10 @@ app.get("/album", (req,res)=>{
 
 app.get("/videos", (req,res)=>{
  res.render(__dirname + '/app/views/index4.ejs')
+})
+
+app.get("/list", (req,res)=>{
+    res.render(__dirname + "/app/views/index5.ejs", {data:data})
 })
 
 app.listen(8081, ()=>{
